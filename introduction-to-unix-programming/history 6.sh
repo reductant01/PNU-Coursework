2f7796c942a06e43eeab931d4a7142e9d59f7636
hwj <hwj07280728@gmail.com>
2026-04-15 10:14:49 +0900

    6주차

---
+++ introduction-to-unix-programming/history 6.sh
@@ -0,0 +1,28 @@
+cmp Hello1.c Hello2.c (Compare)
+
+diff Hello1.c Hello2.c (Difference)
+diff Hello1.c Hello2.c > hello.txt
+
+cat -n Hello1.c (concatenate)
+
+alias ㅣㄴ='ls'
+
+git config --list
+
+git init
+git add .
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/yourusername/yourrepository.git
+git push -u origin main
+
+git status
+
+git branch
+git branch -m main (move)
+git branch -M main (move force)
+
+git log
+git log --oneline
+
+git fetch
